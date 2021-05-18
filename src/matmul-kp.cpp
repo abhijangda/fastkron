b625a1e122c9a7198f2b610135b6ab0deb1e05d2
Abhinav Jangda <abhijangda@gmail.com>
2021-05-18 16:19:33 -0400

    initial version of slicedmatmul

--- src/matmul-kp.cpp
+++ src/matmul-kp.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <cassert>
 
 void setMatrix(int* mat, int M, int N, int v) 
 {
@@ -15,7 +16,7 @@ void printMatrix(int* mat, int M, int N)
 {
   for (int i = 0; i < M; i++) {
     for (int j = 0; j < N; j++) {
-      printf("v %d\n", mat[i*N + j]);
+      printf("mat[%d*N + %d] = %d\n", i,j,mat[i*N + j]);
     }
   }
 }
@@ -40,7 +41,28 @@ void baselineKPThenMatmul(int* result, int* x, int* kpout, int* kpMats[NUM_KP_MA
       }    
     }    
   }
+}
 
+/**
+ * 
+*/
+template<int NUM_KP_MATS>
+void slicedMatmul(int* kpMatmulResult[NUM_KP_MATS], int* x, int* kpout, int* kpMats[NUM_KP_MATS],
+                  int M, int N, int K, int KP_MAT_M, int KP_MAT_N, int KP_MAT_K) {
+  assert (M == N && N == K);
+  for (int kp = 0; kp < NUM_KP_MATS; kp++) {
+    int* prevKPMatmul = (kp == 0) ? x : kpMatmulResult[kp - 1];
+    for (int i = 0; i < M; i++) {
+      for (int j = 0; j < N; j++) {
+        int r = 0;
+        for (int kp_k = 0; kp_k < KP_MAT_K; kp_k++) {
+          r += prevKPMatmul[i*K + (j*KP_MAT_K)%K + kp_k] * kpMats[1][kp_k*KP_MAT_K + j % KP_MAT_N];
+        }
+
+        kpMatmulResult[kp][i*K + j] = r;
+      }
+    }
+  }
 }
 
 int main(int argc, char* argv[]) 
@@ -59,17 +81,24 @@ int main(int argc, char* argv[])
   setMatrix(x, M, K, 1);
 
   int *kpMats[NUM_KP_MATS];
+  int* kpMatmulResult[NUM_KP_MATS];
 
   for (int i = 0; i < NUM_KP_MATS; i++) {
     kpMats[i] = new int[KP_MAT_K * KP_MAT_N];
     setMatrix(kpMats[i], KP_MAT_K, KP_MAT_N, 1);
+    kpMatmulResult[i] = new int[M*N];
   }
 
   int* result = new int[M*N];
 
-  baselineKPThenMatmul<NUM_KP_MATS>(result, x, kpout, kpMats, 
-                          M, N, K, KP_MAT_M, KP_MAT_N, KP_MAT_K);
-  // printMatrix(result, M, N);
+  // baselineKPThenMatmul<NUM_KP_MATS>(result, x, kpout, kpMats, 
+  //                         M, N, K, KP_MAT_M, KP_MAT_N, KP_MAT_K);
+
+  
+
+  slicedMatmul<NUM_KP_MATS>(kpMatmulResult, x, kpout, kpMats,
+                  M, N, K, KP_MAT_M, KP_MAT_N, KP_MAT_K);
+  printMatrix(kpMatmulResult[1], M, N);
 
   return 0;
 }
