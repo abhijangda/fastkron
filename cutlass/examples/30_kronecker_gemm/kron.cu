338476121e6909aa9de65ec356878af6933ff193
Abhinav Jangda <abhijangda@gmail.com>
2022-02-01 15:41:08 -0500

    decrease sh memory loads for KP

--- cutlass/examples/30_kronecker_gemm/kron.cu
+++ cutlass/examples/30_kronecker_gemm/kron.cu
@@ -248,8 +248,8 @@ using SmArch = cutlass::arch::Sm61;
 
 template<typename T,int N_THREADS, int K, int N_COARSE_TB, int TILE_Y, int TILE_X, int TILE_K, int KP_N, int KP_K, int KP_K_BATCH>
 __global__ 
-void cuda_gemm(int M, int N, T * A, T * kron_fac, T * C) {
-  __shared__ int kron_fac_sh[KP_K][TILE_Y];
+void __launch_bounds__(128)  cuda_gemm(int M, int N, T * A, T * kron_fac, T * C) {
+  __shared__ int kron_fac_sh[TILE_Y][KP_K+1];//TODO: Change padding based on value of KP_K and TILE_Y
   __shared__ int As[TILE_X][KP_K][TILE_K/KP_K+1]; //TODO: Change padding based on the value of KP_K
   __shared__ int Csh[TILE_X][K];
 
@@ -258,7 +258,7 @@ void cuda_gemm(int M, int N, T * A, T * kron_fac, T * C) {
   int blockWarps = blockDim.x/warpSize;
 
   for (auto i = threadIdx.x; i < KP_K * TILE_Y; i += blockDim.x) {
-    kron_fac_sh[i/TILE_Y][i%TILE_Y] = kron_fac[(i/TILE_Y) * KP_N + blockIdx.y *TILE_Y+ (i%TILE_Y)];
+    kron_fac_sh[i%TILE_Y][i/TILE_Y] = kron_fac[(i/TILE_Y) * KP_N + blockIdx.y *TILE_Y+ (i%TILE_Y)];
   }
 
   for (int start_row = blockIdx.x * TILE_X; start_row < gridDim.x * TILE_X * N_COARSE_TB; start_row += gridDim.x * TILE_X) {
@@ -274,13 +274,18 @@ void cuda_gemm(int M, int N, T * A, T * kron_fac, T * C) {
 
       for (int a_row = 0; a_row < TILE_X; a_row++) {
         for (int kp_col = wid; kp_col < KP_N; kp_col += blockWarps) {
+          register int kron_fac_r; //TODO: Specifically for KP_K=32 and TILE_Y=32
+
+          kron_fac_r = kron_fac_sh[kp_col][lane];
+
           for (int a_col_start = lane * KP_K; a_col_start < TILE_K; a_col_start += warpSize * KP_K) {
             int c = 0;
-            
+
+            #pragma unroll
             for (int a_col = 0; a_col < KP_K; a_col++) {
               int a = As[a_row][a_col][a_col_start/KP_K];
               int kp_row = a_col;
-              int kp = kron_fac_sh[kp_row][kp_col];
+              int kp = __shfl_sync(0xffffffff, kron_fac_r, a_col);
 
               c += a * kp;
             }
@@ -314,7 +319,7 @@ void customKronGEMM(int NUM_KP_MATS, int* kpMatmulResult[], int* x, int* kpMats[
     const int TILE_Y = 32; //Y direction corresponds to tile of column of the KP factor
     const int TILE_X = 1; //X direction correspond to tile of row 
     const int KP_K_BATCH = 1;
-    const int N_COARSE_TB = 1;
+    const int N_COARSE_TB = 4;
     const int TILE_K = 1024;
 
     dim3 grid = {M/TILE_X/N_COARSE_TB, (N/KP_MAT_N[NUM_KP_MATS-i-1])/TILE_Y}; 
