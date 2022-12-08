a919a4de2d9c2c98d4568ed3b726f9fdbd2426ea
Abhinav Jangda <abhijangda@gmail.com>
2022-12-08 03:58:52 +0000

    results for M=16

--- kronecker-model.py
+++ kronecker-model.py
@@ -195,7 +195,7 @@ def doTorchKron(twoPower, npoints, d):
     return all_cublas_times, all_at_times, all_cuda_times
 
 maxD = {2:22, 4:11, 8:7, 16:6, 32: 5, 64 : 4, 128: 4, 256: 3, 512: 3, 1024:3}
-cases = [{"npoints": 80, "2^l": j, "d": i} for j in maxD for i in range(2 if j > 4 else 4, maxD[j])]
+cases = [{"npoints": 16, "2^l": j, "d": i} for j in maxD for i in range(2 if j > 4 else 4, maxD[j])]
 #  [       {"npoints": 100, "2^l": 32, "d": 2},
 #         {"npoints": 10, "2^l": 32, "d": 2},
 #         {"npoints": 1, "2^l": 32, "d": 2},
