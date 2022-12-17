9702ce250d7c3707126f1824b92324be55450fa7
Abhinav Jangda <abhijangda@gmail.com>
2022-12-17 02:57:21 +0000

    running torch_kron.py for 2 sizes

--- src/torch_kron.py
+++ src/torch_kron.py
@@ -1,38 +1,54 @@
 import torch
+import gpytorch as gp
 
 def initmat(m, n):
-    return torch.randn(m,n)
+    return torch.randint(0, 5, (m,n))
 
 def baseline(input, kronmats):
-    outputKron = kronmats[0]
-    for m in kronmats[1:]:
-        outputKron = torch.kron(outputKron, m)
-    return torch.matmul(input, outputKron)
+    kronmats = gp.lazy.KroneckerProductLazyTensor(*kronmats)        
+    print(kronmats.shape)
+    print(input.shape)
+    return input@kronmats
+    # outputKron = kronmats[0]
+    # for m in kronmats[1:]:
+    #     outputKron = torch.kron(outputKron, m)
+    # return torch.matmul(input, outputKron)
 
 def matmulkron(input, kronmats):
-    output = input
     shape = input.shape
-    
-    for k in reversed(kronmats):
-        newinput = output.reshape(shape[0] * (shape[1]//k.shape[0]), k.shape[0])
-        output = torch.matmul(newinput, k)
-        output = output.view(shape[0], (shape[1]//k.shape[0]), k.shape[0])
-        output = output.transpose(1,2)
-    return output.reshape(shape)
+    input = input.T
+    output = input
+    print(20, shape)
+    for k in kronmats:
+        newinput = output.reshape(k.shape[0], shape[0] * (shape[1]//k.shape[0]))
+        output = torch.matmul(k.T, newinput)
+        # print(output)
+        print(24, output.shape, k.shape, newinput.shape)
+        output = output.view(k.shape[0], (shape[1]//k.shape[0]), shape[0])
+        print(25, output.shape)
+        output = output.transpose(-3,-2)
+        print(28, output.shape)
+        output = output.reshape(*[], -1, shape[0])
+        print(30, output.shape)
+        print(output)
+    return output.mT
 
 
 if __name__ == "__main__":
     npoints = 2
-    twoPower = 8
-    input = initmat(npoints, twoPower**3)
+    twoPower = 3
+    d = 2
+    input = initmat(npoints, twoPower**d)
     kronmats = []
-    for s in range(3):
+    for s in range(d):
         kronmats += [initmat(twoPower,twoPower)]
     # print(kronmats[0])
     # print(kronmats[1])
-    b = baseline(input, kronmats)
+    b = baseline(torch.clone(input), [torch.clone(k) for k in kronmats])
 
-    o = matmulkron(input, kronmats)
-
-    print ((b == o))
-    print ((b == o).all())
+    o = matmulkron(torch.clone(input), kronmats)
+    print('b.shape',b.shape)
+    print (b)
+    print('o.shape',o.shape)
+    print (o)
+    print((b==o).all())
