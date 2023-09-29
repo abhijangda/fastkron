60733cc71e09db306baf6c001691c9a27d6f845f
Abhinav Jangda <abhijangda@gmail.com>
2023-09-29 17:27:59 +0000

    update tuner test

--- tests/multi-gpu-tuner-tests.cu
+++ tests/multi-gpu-tuner-tests.cu
@@ -1,8 +1,8 @@
 #include "gtest/gtest.h"
 #include "testBase.h"
 
-#define MULTI_GPU_NO_FUSION_TEST(M, Facs, FacSize, GM, GK, KronBatch, Type) \
-  TEST(MultiGpuNoFusion, Type##_##M##x##Facs##x##FacSize##x##GM##x##GK##x##KronBatch##_) { \
+#define MULTI_GPU_TUNER_TEST(M, Facs, FacSize, GM, GK, KronBatch, Type) \
+  TEST(MultiGpuTuner, Type##_##M##x##Facs##x##FacSize##x##GM##x##GK##x##KronBatch##_) { \
   uint KP_MAT_N[Facs];\
   uint KP_MAT_K[Facs];\
   uint N = 1;\
@@ -12,10 +12,10 @@
     K *= FacSize;\
     KP_MAT_K[i] = KP_MAT_N[i] = FacSize;\
   }\
-  bool b = run<Type>(M, N, K, Facs, KP_MAT_N, KP_MAT_K, 0, 0, false, GM, GK, GM*GK, KronBatch, true, false, false, false);\
+  bool b = run<Type>(M, N, K, Facs, KP_MAT_N, KP_MAT_K, 0, 0, false, GM, GK, GM*GK, KronBatch, true, true, true, false);\
   EXPECT_TRUE(b);\
 }
 
-MULTI_GPU_NO_FUSION_TEST(4, 5, 16, 2, 1, 4, float);
-MULTI_GPU_NO_FUSION_TEST(8, 4, 16, 4, 2, 3, float);
-MULTI_GPU_NO_FUSION_TEST(8, 4, 16, 2, 2, 2, float);
+MULTI_GPU_TUNER_TEST(4, 5, 16, 2, 1, 4, float);
+MULTI_GPU_TUNER_TEST(8, 4, 16, 4, 2, 3, float);
+MULTI_GPU_TUNER_TEST(8, 4, 16, 2, 2, 2, float);
