e153633913032797c791bab1d85842bfedae5054
Abhinav Jangda <abhijangda@gmail.com>
2023-10-20 02:34:03 +0000

    multi gpu bench script

--- benchmarks/run_kron_matmuls.py
+++ benchmarks/run_kron_matmuls.py
@@ -16,14 +16,21 @@ def gen_kernels(shape):
 def build_kron():
   run_command("make kron -j")
 
-def run_kron(shape):
+def run_kron(shape, GM, GK, LocalKrons):
   kron = f"./kron -m {shape.m} -n {shape.n} -p {shape.ps[0]} -q {shape.qs[0]} -r 20 -w 10 -t float --tune"
-  o = run_command(kron)
-  wofuse = re.findall(r"GFLOPS\: ([\d\.]+)", o)[0]
+  if GM * GK != 1:
+    kron += f" --gpus {GM*GK} --GM {GM} --GK {GK} --gpuLocalKrons {LocalKrons}"
+
   o = run_command(kron + " --fuse")
   fused = re.findall(r"GFLOPS\: ([\d\.]+)", o)[0]
   
-  print(shape, wofuse, fused)
+  if shape.ps[0] <= 32:
+    o = run_command(kron)
+    wofuse = re.findall(r"GFLOPS\: ([\d\.]+)", o)[0]
+  else:
+    wofuse = fused
+
+  print(shape, GM*GK, wofuse, fused)
 
 class Shape:
   def __init__(self, m, n, p, q):
@@ -35,15 +42,38 @@ class Shape:
   def __repr__(self):
     return f"{self.m}_{self.ps[0]}x{self.qs[0]}^{self.n}"
 
-M = 1024
+def run_single_gpu():
+  M = 1024
+  cases = [Shape(M, 5, 8, 8),     Shape(M, 6, 8, 8),
+           Shape(M, 4, 16, 16),   Shape(M, 5, 16, 16),
+           Shape(M, 3, 32, 32),   Shape(M, 4, 32, 32),
+           Shape(M, 2, 64, 64),   Shape(M, 3, 64, 64),
+           Shape(M, 2, 128, 128), Shape(M, 3, 128, 128)]
 
-cases = [Shape(M, 5, 8, 8),     Shape(M, 6, 8, 8),
-         Shape(M, 4, 16, 16),   Shape(M, 5, 16, 16),
-         Shape(M, 3, 32, 32),   Shape(M, 4, 32, 32),
-         Shape(M, 2, 64, 64),   Shape(M, 3, 64, 64),
-         Shape(M, 2, 128, 128), Shape(M, 3, 128, 128)]
+  for shape in cases:
+    gen_kernels(shape)
+    build_kron()
+    run_kron(shape, 1, 1, 1)
+  
+def multi_gpu():
+  cases = []
+  M_64 = 64
+  cases += [Shape(M_64, 4, 64, 64)]
+  M_128 = 4
+  cases += [Shape(M_128, 4, 128, 128)]
+  
+  run_command("make gen-multi-gpu-tests-kernel")
 
-for shape in cases:
-  gen_kernels(shape)
   build_kron()
-  run_kron(shape)
+
+  for shape in cases:
+    GMs = [1, 2, 2, 4, 4]
+    GKs = [1, 1, 2, 2, 4]
+    for j,gpus in enumerate([1, 2, 4, 8, 16]):
+      gm = GMs[j]
+      gk = GKs[j]
+      shapeGM = Shape(shape.m * gm, shape.n, shape.ps[0], shape.qs[0])
+      LocalKrons = shapeGM.n if gk == 1 else shapeGM.n - 2
+      run_kron(shapeGM, gm, gk, LocalKrons)
+
+multi_gpu()
