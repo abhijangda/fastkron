6487aa8e67e90d688080f52b9256a367bd1f2541
Abhinav Jangda <abhijangda@gmail.com>
2024-01-16 20:14:15 +0000

    stackarray.h

--- src/kmm/matrix.h
+++ src/kmm/matrix.h
@@ -2,6 +2,7 @@
 #include <cassert>
 #include <functional>
 
+#include "stackarray.h"
 #include "config.h"
 
 #pragma once
@@ -94,6 +95,7 @@ public:
   }
 };
 
+//TODO: Think about this
 template<typename T>
 class Slice {
   const Matrix parent;
@@ -151,58 +153,7 @@ protected:
 };
 
 //TODO: StackArray.h
-template<typename T, uint32_t MaxSize>
-class StackArray {
-public:
-  T array[MaxSize];
-  uint32_t n;
 
-  StackArray() {
-    for (uint32_t i = 0; i < MaxSize; i++) {
-      array[i] = T();
-    }
-  }
-
-public:
-  StackArray(const T* ptrs, uint32_t n) : n(n) {
-    if (ptrs) {
-      for (uint32_t i = 0; i < n; i++) {
-        array[i] = ptrs[i];
-      }
-    }
-
-    for (uint32_t i = n; i < MaxSize; i++) {
-      array[i] = T();
-    }
-  }
-
-  CUDA_DEVICE_HOST
-  T& operator[](int index) {
-    assert (index < n && index >= 0);
-    return array[index];
-  }
-
-  CUDA_DEVICE_HOST
-  T& operator[](uint32_t index) {
-    assert (index < n);
-    return array[index];
-  }
-
-  StackArray<T, MaxSize> sub(uint32_t start, uint32_t len) const {
-    assert(len <= n);
-    T ptrs[len];
-    for (uint32_t i = 0; i < len; i++) {
-      ptrs[i] = array[i + start];
-    }
-
-    return StackArray<T, MaxSize>(ptrs, len);
-  }
-
-  CUDA_DEVICE_HOST
-  uint32_t len() const {return n;}
-
-  StackArray(const StackArray& x) : StackArray (&x.array[0], x.len()) {}
-};
 
 template<uint32_t MaxSize>
 class FactorArray : public StackArray<Factor, MaxSize> {
