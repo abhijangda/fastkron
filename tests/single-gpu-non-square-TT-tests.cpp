d45565dfc4448cb07e373929861671620c8d7fd7
Abhinav Jangda <abhijangda@gmail.com>
2024-02-12 23:17:42 +0000

    add single gpu non square TT tests

---
+++ tests/single-gpu-non-square-TT-tests.cpp
@@ -0,0 +1,26 @@
+#include "gtest/gtest.h"
+#include "testBase.h"
+
+#define NON_SQUARE_SINGLE_GPU(M, Facs, P, Q, Type) \
+  TEST(NonSquareSingleGPU, Type##_##M##x##Facs##x##P##x##Q##_) { \
+  uint KP_MAT_N[Facs];\
+  uint KP_MAT_K[Facs];\
+  uint N = 1;\
+  uint K = 1;\
+  for (uint i = 0; i < (uint)Facs; i++) {\
+    N *= Q;\
+    K *= P;\
+    KP_MAT_K[i] = P;\
+    KP_MAT_N[i] = Q;\
+  }\
+  bool b = run<Type>(M, N, K, Facs, KP_MAT_N, KP_MAT_K, fastKronOp_T, fastKronOp_T, 1, 0, false, 1, 1, 1, 1, true, false, true, false);\
+  EXPECT_TRUE(b);\
+}
+
+NON_SQUARE_SINGLE_GPU(11, 4, 8, 16, float)
+NON_SQUARE_SINGLE_GPU(12, 5, 8, 16, float)
+
+NON_SQUARE_SINGLE_GPU(12, 3, 128, 32, float)
+
+NON_SQUARE_SINGLE_GPU(11, 3, 32, 16, float)
+NON_SQUARE_SINGLE_GPU(12, 4, 32, 16, float)
