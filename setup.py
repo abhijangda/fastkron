40688f71aa512fbeda869e442ab2f9b89cf79e91
Abhinav Jangda <abhijangda@gmail.com>
2024-02-13 04:04:27 +0000

    add Cmake extension to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,32 @@
-from distutils.core import setup, Extension
-import distutils
-distutils.log.set_verbosity(0)
-setup(name='fastkroncpp', version='1.0', \
-   ext_modules=[Extension('fastkroncpp', ['src/pymodule.cpp'], include_dirs=["/usr/local/cuda/include/"], 
-                          library_dirs=["/home/saemal/KroneckerGPU"], libraries = ["Kron"])])
+from pathlib import Path
+import shutil
+import subprocess
+import os
 
+from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext
+
+class CMakeExtension(Extension):
+   def __init__(self) -> None:
+      super().__init__("FastKronNative", sources=[])
+
+class CMakeBuild(build_ext):
+   def build_extension(self, ext: CMakeExtension) -> None:
+      self.build_dir = Path("build/").resolve()
+      if self.build_dir.exists():
+         shutil.rmtree(self.build_dir)
+      self.build_dir.mkdir()
+      # os.chdir(self.build_dir)
+      subprocess.run(
+         ["cmake", ".."], cwd=self.build_dir, check=True
+      )
+      subprocess.run(
+         ["make", "FastKron", "-j"], cwd=self.build_dir, check=True
+      )
+      # subprocess.getstatusoutput("cmake ..")
+      # subprocess.getstatusoutput("make FastKron")
+      # os.chdir("../")
+      
+setup(name='fastkroncpp', version='1.0', \
+      ext_modules=[CMakeExtension()],
+      cmdclass={"build_ext": CMakeBuild})
