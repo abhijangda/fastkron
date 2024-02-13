0d34e9820c4b55d620d9d78b24ac39b933b03c51
Abhinav Jangda <abhijangda@gmail.com>
2024-02-13 06:22:47 +0000

    python module directory

--- setup.py
+++ setup.py
@@ -8,25 +8,21 @@ from setuptools.command.build_ext import build_ext
 
 class CMakeExtension(Extension):
    def __init__(self) -> None:
-      super().__init__("FastKronNative", sources=[])
+      super().__init__("libFastKron", sources=[])
 
 class CMakeBuild(build_ext):
    def build_extension(self, ext: CMakeExtension) -> None:
       self.build_dir = Path("build/").resolve()
+      ext_fullpath = Path.cwd() / self.get_ext_fullpath(ext.name)
+
       if self.build_dir.exists():
          shutil.rmtree(self.build_dir)
       self.build_dir.mkdir()
       # os.chdir(self.build_dir)
-      subprocess.run(
-         ["cmake", ".."], cwd=self.build_dir, check=True
-      )
-      subprocess.run(
-         ["make", "FastKron", "-j"], cwd=self.build_dir, check=True
-      )
-      # subprocess.getstatusoutput("cmake ..")
-      # subprocess.getstatusoutput("make FastKron")
-      # os.chdir("../")
+      subprocess.run(["cmake", ".."],            cwd=self.build_dir, check=True)
+      subprocess.run(["make", "FastKron", "-j"], cwd=self.build_dir, check=True)
+      shutil.copy(Path.cwd() / "build/libFastKron.so", ext_fullpath)
       
-setup(name='fastkroncpp', version='1.0', \
-      ext_modules=[CMakeExtension()],
-      cmdclass={"build_ext": CMakeBuild})
+setup(name='pyfastkron', version='1.0')
+      # ext_modules=[CMakeExtension()],
+      # cmdclass={"build_ext": CMakeBuild})
