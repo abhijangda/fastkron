bd93297bb2d2c554238751ad087abb80fcf0a534
Abhinav Jangda <abhijangda@gmail.com>
2024-03-16 02:13:58 +0000

    add hip kernel db

---
+++ src/kernel_db/hip_kernel_db.h
@@ -0,0 +1,65 @@
+#include "kernel_db.h"
+
+#include "kmm/kmmalgo.h"
+#include "kernels/kernel_info.h"
+#include "kernels/params.h"
+#include "kernel_db/kernel_db.h"
+#include "env/env.h"
+#include "utils/thread_pool.h"
+
+class HIPKernelDatabase : public KernelDatabase {
+public:
+  std::vector<void*> streams;
+  // uint numGPUs_;
+  // uint gpusInM_;
+  // uint gpusInK_;
+  // uint perGPUKronBatch_;
+  // bool isDistributed_;
+  // DistComm distComm_;
+  // std::vector<ncclComm_t> ncclComms;
+  // pthread_barrier_t* barriers_;
+  // thread_pool<ThreadArgs*>* threads_;
+
+public:
+  HIPKernelDatabase() {}
+  ~HIPKernelDatabase() {}
+
+  fastKronError init(void* ptrToStream, int gpus, int gpusInM, int gpusInK, int gpuKrons){}
+  
+  void free() {
+    streams.clear();
+    // if (isDistributed_) {
+    //   for (uint g = 0; g < gpusInM_; g++) {
+    //     int s = pthread_barrier_destroy(&barriers_[g]);
+    //     PTHREAD_BARRIER_CHECK(s);
+    //   }
+
+    //   delete threads_;
+    //   delete barriers_;
+
+    //   if (distComm_ == DistComm::NCCL) {
+    //     for (int i=0; i<ncclComms.size(); i++)
+    //       ncclCommDestroy(ncclComms[i]);
+    //   }
+    // }
+  }
+
+  virtual fastKronError invokeKernel(KernelInfo* kernelInfo, const uint kronIndex, 
+                                   KMMProblem problem,
+                                   EpilogueParams epilogueParams,
+                                   KernelMode execMode) {}
+  virtual fastKronError invokeP2PStoreKernel(KernelInfo* kernelInfo, const uint kronIndex, 
+                                           KMMProblem problem, DistributedParams distParams, 
+                                           EpilogueParams epilogueParams,
+                                           KernelMode execMode){}
+  virtual fastKronError timeKernel(KernelInfo* kernelInfo, const uint kronIndex, 
+                                 KMMProblem problem, DistributedParams distParams, 
+                                 EpilogueParams epilogueParams,
+                                 KernelMode execMode, 
+                                 bool distP2PStore,
+                                 int warmups, int runs,
+                                 float& runtime){}
+  virtual fastKronError procMalloc(uint32_t proc, size_t size, void*& ptr);
+  virtual fastKronError procMemset(uint32_t proc, Matrix& m, float val){}
+  virtual fastKronError procFree(uint32_t proc, void* ptr){}
+};
