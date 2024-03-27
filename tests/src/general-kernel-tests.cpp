26ca5ead56e9ffd47598567f028cfcad333825d0
Abhinav Jangda <abhijangda@gmail.com>
2024-03-27 19:54:29 +0000

    testBase printf

--- tests/src/general-kernel-tests.cpp
+++ tests/src/general-kernel-tests.cpp
@@ -21,35 +21,36 @@
   EXPECT_TRUE(result);\
 }
 
-FUSION_TEST(1, 2, 10, 2, 2, float);
-FUSION_TEST(1, 2, 8, 3, 3, float);
-FUSION_TEST(1, 2, 8, 4, 4, float);
-FUSION_TEST(1, 2, 7, 5, 5, float);
-FUSION_TEST(1, 2, 7, 6, 6, float);
-FUSION_TEST(1, 2, 7, 8, 8, float);
-FUSION_TEST(1, 2, 6, 12, 12, float);
-FUSION_TEST(1, 2, 6, 16, 16, float);
-FUSION_TEST(1, 2, 5, 24, 24, float);
-FUSION_TEST(1, 2, 4, 31, 31, float);
-FUSION_TEST(1, 2, 4, 32, 32, float);
-FUSION_TEST(1, 2, 3, 50, 50, float);
-FUSION_TEST(1, 2, 3, 55, 55, float);
-FUSION_TEST(1, 2, 3, 62, 62, float);
-FUSION_TEST(1, 2, 3, 64, 64, float);
-FUSION_TEST(1, 2, 3, 127, 127, float);
-FUSION_TEST(1, 2, 3, 128, 128, float);
-FUSION_TEST(1, 2, 3, 129, 129, float);
-FUSION_TEST(1, 2, 3, 255, 255, float);
-FUSION_TEST(1, 2, 2, 297, 297, float);
-FUSION_TEST(1, 2, 2, 384, 384, float);
-FUSION_TEST(1, 2, 2, 505, 505, float);
-FUSION_TEST(1, 2, 2, 512, 512, float);
-FUSION_TEST(1, 2, 2, 739, 739, float);
-FUSION_TEST(1, 2, 2, 1024, 1024, float);
+#define M_ 32
+FUSION_TEST(M_, 2, 10, 2, 2, float);
+FUSION_TEST(M_, 2, 8, 3, 3, float);
+FUSION_TEST(M_, 2, 8, 4, 4, float);
+FUSION_TEST(M_, 2, 7, 5, 5, float);
+FUSION_TEST(M_, 2, 7, 6, 6, float);
+FUSION_TEST(M_, 2, 7, 8, 8, float);
+FUSION_TEST(M_, 2, 6, 12, 12, float);
+FUSION_TEST(M_, 2, 6, 16, 16, float);
+FUSION_TEST(M_, 2, 5, 24, 24, float);
+FUSION_TEST(M_, 2, 4, 31, 31, float);
+FUSION_TEST(M_, 2, 4, 32, 32, float);
+FUSION_TEST(M_, 2, 3, 50, 50, float);
+FUSION_TEST(M_, 2, 3, 55, 55, float);
+FUSION_TEST(M_, 2, 3, 62, 62, float);
+FUSION_TEST(M_, 2, 3, 64, 64, float);
+FUSION_TEST(M_, 2, 3, 127, 127, float);
+FUSION_TEST(M_, 2, 3, 128, 128, float);
+FUSION_TEST(M_, 2, 3, 129, 129, float);
+FUSION_TEST(M_, 2, 3, 255, 255, float);
+FUSION_TEST(M_, 2, 2, 297, 297, float);
+FUSION_TEST(M_, 2, 2, 384, 384, float);
+FUSION_TEST(M_, 2, 2, 505, 505, float);
+FUSION_TEST(M_, 2, 2, 512, 512, float);
+FUSION_TEST(M_, 2, 2, 739, 739, float);
+FUSION_TEST(M_, 2, 2, 1024, 1024, float);
 
 
-FUSION_TEST(1, 2, 4, 31, 63, float);
-FUSION_TEST(1, 2, 4, 63, 31, float);
-FUSION_TEST(1, 2, 3, 297, 127, float);
-FUSION_TEST(1, 2, 3, 127, 297, float);
-FUSION_TEST(1, 2, 2, 936, 505, float);
+FUSION_TEST(M_, 2, 4, 31, 63, float);
+FUSION_TEST(M_, 2, 4, 63, 31, float);
+FUSION_TEST(M_, 2, 3, 297, 127, float);
+FUSION_TEST(M_, 2, 3, 127, 297, float);
+FUSION_TEST(M_, 2, 2, 936, 505, float);
