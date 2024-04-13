416396a7956f66f3178831a4bbf981a7f620d98e
Abhinav Jangda <abhijangda@gmail.com>
2024-04-13 21:39:18 +0000

    fix multi gpu cuda tests

--- tests/src/multi-cuda-no-fusion-non-square-tests.cpp
+++ tests/src/multi-cuda-no-fusion-non-square-tests.cpp
@@ -13,14 +13,14 @@
     KP_MAT_K[i] = P;\
     KP_MAT_N[i] = Q;\
   }\
-  bool b = run<Type>(M, N, K, Facs, KP_MAT_N, KP_MAT_K, fastKronOp_N, fastKronOp_N, 1, 0, false, 1, 1, 1, 1, true, false, true, fastKronBackend_CUDA, false);\
+  bool b = run<Type>(M, N, K, Facs, KP_MAT_N, KP_MAT_K, fastKronOp_N, fastKronOp_N, 1, 0, false, GM, GK, GM*GK, KronBatch, true, false, true, fastKronBackend_CUDA, false);\
   EXPECT_TRUE(b);\
 }
 
 MULTI_GPU_NON_SQUARE_TEST(18, 5, 8, 32, 2, 1, 5, float);
 MULTI_GPU_NON_SQUARE_TEST(14, 5, 8, 32, 1, 2, 3, float);
-MULTI_GPU_NON_SQUARE_TEST(18, 5, 8, 32, 4, 4, 3, float);
+MULTI_GPU_NON_SQUARE_TEST(18, 5, 8, 32, 2, 2, 3, float);
 
 MULTI_GPU_NON_SQUARE_TEST(18, 4, 64, 16, 2, 1, 4, float);
 MULTI_GPU_NON_SQUARE_TEST(14, 4, 64, 16, 1, 2, 3, float);
-MULTI_GPU_NON_SQUARE_TEST(18, 4, 64, 16, 4, 4, 3, float);
+MULTI_GPU_NON_SQUARE_TEST(18, 4, 64, 16, 2, 2, 3, float);
