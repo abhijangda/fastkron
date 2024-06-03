717aeb41436969faa82c17f8d00fdfc5356ba0dd
Abhinav Jangda <abhijangda@gmail.com>
2024-06-03 00:18:05 +0000

    vector types

--- src/config.h
+++ src/config.h
@@ -3,11 +3,11 @@
         #define __forceinline__ inline
     #endif
 
-    #define CUDA_DEVICE_HOST __host__ __device__ __forceinline__
     #define CUDA_HOST __host__
     #define CUDA_DEVICE __device__ __forceinline__
+    #define CUDA_DEVICE_HOST CUDA_HOST CUDA_DEVICE
 #else
-    #define CUDA_DEVICE_HOST
     #define CUDA_HOST
     #define CUDA_DEVICE
+    #define CUDA_DEVICE_HOST CUDA_HOST CUDA_DEVICE
 #endif
