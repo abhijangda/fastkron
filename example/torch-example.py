81d17ecbe627b83484f166ea2a4f9c5355a65ab0
Abhinav Jangda <abhijangda@gmail.com>
2024-08-03 23:21:05 +0000

    add run-*-tests

--- example/torch-example.py
+++ example/torch-example.py
@@ -1,41 +1,41 @@
 from functools import reduce
 
 try:
-    import torch
-    import pyfastkron.fastkrontorch as fk
+  import torch
+  import pyfastkron.fastkrontorch as fk
 
-    M = 10
-    N = 5
-    Ps = [8] * N
-    Qs = [8] * N
+  M = 10
+  N = 5
+  Ps = [8] * N
+  Qs = [8] * N
 
-    # CUDA example in PyTorch
-    x = torch.ones((M, reduce((lambda a, b: a * b), Ps)), dtype=torch.float32).cuda()
-    y = torch.zeros((M, reduce((lambda a, b: a * b), Qs)), dtype=torch.float32).cuda()
-    fs = [torch.ones((Ps[0], Qs[0]), dtype=torch.float32).cuda() for i in range(0, N)]
+  # CUDA example in PyTorch
+  x = torch.ones((M, reduce((lambda a, b: a * b), Ps)), dtype=torch.float32).cuda()
+  y = torch.zeros((M, reduce((lambda a, b: a * b), Qs)), dtype=torch.float32).cuda()
+  fs = [torch.ones((Ps[0], Qs[0]), dtype=torch.float32).cuda() for i in range(0, N)]
 
-    y = fk.gekmm(x, fs)
+  y = fk.gekmm(x, fs)
 
-    print(y)
+  print(y)
 except:
-    pass
+  pass
 
 try:
-    import numpy as np
-    import pyfastkron.fastkronnumpy as fk
+  import numpy as np
+  import pyfastkron.fastkronnumpy as fk
 
-    M = 10
-    N = 5
-    Ps = [8] * N
-    Qs = [8] * N
+  M = 10
+  N = 5
+  Ps = [8] * N
+  Qs = [8] * N
 
-    # Numpy example
-    x = np.ones((M, reduce((lambda a, b: a * b), Ps)), dtype=np.double)
-    fs = [np.ones((Ps[0], Qs[0]), dtype=np.double) for i in range(0, N)]
-    z = np.ones((M, reduce((lambda a, b: a * b), Qs)), dtype=np.double)
-    y = fk.gekmm(x, fs, alpha=2.0, beta=1.0, z=z)
+  # Numpy example
+  x = np.ones((M, reduce((lambda a, b: a * b), Ps)), dtype=np.double)
+  fs = [np.ones((Ps[0], Qs[0]), dtype=np.double) for i in range(0, N)]
+  z = np.ones((M, reduce((lambda a, b: a * b), Qs)), dtype=np.double)
 
-    print(y)
+  y = fk.gekmm(x, fs, alpha=2.0, beta=1.0, z=z)
+
+  print(y)
 except:
-    pass
-
+  pass
