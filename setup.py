1b454fca876337564b086e13ccc2e028905be7c6
Abhinav Jangda <abhijangda@gmail.com>
2024-12-06 20:11:47 +0000

    u

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ def find_version(*file_paths):
 
 setup(
     packages=['pyfastkron'],
-    ext_modules=[CMakeExtension('pyfastkron.FastKronX86')],
+    ext_modules=[CMakeExtension('pyfastkron.FastKronX86'),
                  CMakeExtension('pyfastkron.FastKronCUDA')],
     version=find_version("pyfastkron", "version.py"),
     cmdclass={
